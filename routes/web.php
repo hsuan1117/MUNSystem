296f035971e7a907febee97071610683768615c5
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-07-08 14:53:30 +0800

    0908

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/1', function () {
+    return "123";
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
