bf7c639e05664a073366896d446eccec185605d1
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-08-21 14:36:33 +0800

    Add note

--- resources/views/app/conference/conference.blade.php
+++ resources/views/app/conference/conference.blade.php
@@ -12,8 +12,9 @@
                         {{gettype($conf_data)}}<br>
                         <a href="{{route('app.conference.role.home',$conf_id)}}" class="btn btn-primary">Conference Roles (Countries)</a>
                         <a href="{{route('app.conference.roleCall.home',$conf_id)}}" class="btn btn-primary">Roll Call</a>
-                        <a href="{{route('app.conference.openingSpeech.home',$conf_id)}}" class="btn btn-primary">Opening Speech</a>
+                        <a href="{{route('app.conference.openingSpeech.home',$conf_id)}}" class="btn btn-primary">Opening Speech</a><br>
                         <a href="{{route('app.conference.amendment.home',$conf_id)}}" class="btn btn-primary">Amendment</a>
+                        <a href="{{route('app.conference.note.home',$conf_id)}}" class="btn btn-primary">Note</a>
                     </div>
                 </div>
             </div>
