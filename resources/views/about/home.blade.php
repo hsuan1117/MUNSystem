c8447eb317a68089a15fb1ddbf84b6ee5bc34932
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-09-03 00:39:22 +0800

    [Add] Add About

--- resources/views/about/home.blade.php
+++ resources/views/about/home.blade.php
@@ -5,13 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('About') }}</div>
+                <div class="card-header">{{ __('About SweetMUN') }}</div>
                 <div class="card-body">
                     Proudly Made with ♥ by <b>Hsuan Design</b>(慢慢)
                     <div class="card">
                         <div class="card-header"><i class="fa fa-commenting-o"></i>&nbsp;What's this?</div>
                         <div class="card-body">
                             <i class="fa fa-pencil-square-o"></i>&nbsp;This is the system for Model United Nation
+                        </div>
+                    </div>
+                    &nbsp;
+                    <div class="card">
+                        <div class="card-header"><i class="fa fa-commenting-o"></i>&nbsp;Who developed this?</div>
+                        <div class="card-body">
+                            <i class="fa fa-pencil-square-o"></i>&nbsp;The system is made by Hsuan.
                         </div>
                     </div>
                 </div>
