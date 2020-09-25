40f6985f80cbe81611c24a4937781081ec2f814e
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-09-25 11:44:44 +0800

    [Modify] Fix file permission

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,8 @@ php artisan migrate:refresh --seed
 php artisan config:clear
 php artisan config:cache
 php artisan cache:clear
+php artisan key:generate
+chown www-data:www-data -R *
+chmod 755 -R *
 npm install --production
 npm run dev
